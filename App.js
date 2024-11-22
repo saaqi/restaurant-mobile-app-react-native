4d3f550e5e17ddb2eee4ef7702e543e44086e65b
Saqib Islam <saaqi.grw@gmail.com>
2024-11-22 15:22:13 +0500

    Enhance App layout with SafeAreaView and dark mode support; update expo version and add workspace recommendations

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, useColorScheme } from 'react-native';
+
 
 export default function App() {
+  const isDark = useColorScheme() === 'dark';
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World! This is Saqib Islam.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[
+      styles.container,
+      {
+        backgroundColor: isDark ? '#1A1A19' : '#F6FCDF'
+      }
+    ]}>
+      <View style={styles.innerContainer}>
+        <Text style={{ color: isDark ? '#F6FCDF' : '#1A1A19' }}>Hello World! This is Saqib Islam.</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+      alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  innerContainer: {
+    padding: 20,
   },
 });
